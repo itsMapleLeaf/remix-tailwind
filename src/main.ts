4fa782b9a3fa9ee17038cc9fee54685f03f3057c
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-11-24 09:04:22 -0600

    cache by absolute file path

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Response } from "@remix-run/node"
 import { readFile } from "fs/promises"
+import { isAbsolute, join } from "path"
 import postcss from "postcss"
 import tailwindcss from "tailwindcss"
 
@@ -13,7 +14,7 @@ const cache = new Map<string | symbol, string>()
 const defaultCacheKey = Symbol("remix-tailwind-default")
 
 export async function serveTailwindCss(cssFilePath?: string) {
-  const cacheKey = cssFilePath || defaultCacheKey
+  const cacheKey = cssFilePath ? toAbsolutePath(cssFilePath) : defaultCacheKey
   const cachedResponse = cache.get(cacheKey)
   if (process.env.NODE_ENV === "production" && cachedResponse) {
     return cssResponse(cachedResponse)
@@ -38,4 +39,11 @@ function cssResponse(css: string): Response {
   return new Response(css, {
     headers: { "content-type": "text/css" },
   })
+}
+
+function toAbsolutePath(path: string): string {
+  if (isAbsolute(path)) {
+    return path
+  }
+  return join(process.cwd(), path)
 }
